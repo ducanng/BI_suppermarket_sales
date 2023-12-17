cb7379bb88a1200fc1a7a755ac5e0d05263532b6
Ducan Nguyen <ducan172002@gmail.com>
2023-12-18 00:09:34 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -43,7 +43,9 @@ create table dbo.nds_product
         primary key,
     ProductNK     nvarchar(255)
         unique,
-    ProductLineID bigint,
+    ProductLineID bigint
+        constraint nds_product_nds_productLine_ProductLineID_fk
+            references dbo.nds_productLine,
     UnitPrice     float,
     sourceID      int,
     CreatedAt     datetime,
@@ -78,8 +80,12 @@ create table dbo.nds_invoice
     InvoiceID  bigint identity
         primary key,
     InvoiceNK  nvarchar(255),
-    CityID     bigint,
-    CustomerID bigint,
+    CityID     bigint
+        constraint nds_invoice_nds_city_CityID_fk
+            references dbo.nds_city,
+    CustomerID bigint
+        constraint nds_invoice_nds_customer_CustomerID_fk
+            references dbo.nds_customer,
     DTime      datetime,
     Payment    nvarchar(255),
     Rating     float,
@@ -91,8 +97,12 @@ create table dbo.nds_invoiceDetail
 (
     InvoiceDetailID       bigint identity
         primary key,
-    InvoiceID             bigint,
-    ProductID             bigint,
+    InvoiceID             bigint
+        constraint nds_invoiceDetail_nds_invoice_InvoiceID_fk
+            references dbo.nds_invoice,
+    ProductID             bigint
+        constraint nds_invoiceDetail_nds_product_ProductID_fk
+            references dbo.nds_product,
     Quantity              float,
     Tax                   float,
     Total                 float,
